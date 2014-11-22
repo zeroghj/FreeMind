6762a6b9f2a9d4ba53db75835fd7e719223ad13b
Eric Bozikian <ericnbozikian@msn.com>
2014-11-22 03:50:16 -0500

    Finished copying over all the methods related to Menus.

--- freemind/modes/mindmapmode/MindMapMenuController.java
+++ freemind/modes/mindmapmode/MindMapMenuController.java
@@ -1,15 +1,22 @@
 package freemind.modes.mindmapmode;
 
+import java.awt.Component;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
 import java.util.logging.Logger;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.ButtonGroup;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.KeyStroke;
 
 import org.jibx.runtime.IUnmarshallingContext;
 import org.jibx.runtime.JiBXException;
@@ -17,8 +24,24 @@ import org.jibx.runtime.JiBXException;
 import freemind.common.XmlBindingTools;
 import freemind.controller.MenuBar;
 import freemind.controller.StructuredMenuHolder;
+import freemind.controller.actions.generated.instance.MenuActionBase;
+import freemind.controller.actions.generated.instance.MenuCategoryBase;
+import freemind.controller.actions.generated.instance.MenuCheckedAction;
+import freemind.controller.actions.generated.instance.MenuRadioAction;
+import freemind.controller.actions.generated.instance.MenuSeparator;
 import freemind.controller.actions.generated.instance.MenuStructure;
+import freemind.controller.actions.generated.instance.MenuSubmenu;
+import freemind.main.Tools;
+import freemind.modes.ControllerAdapter;
+import freemind.modes.MindMapNode;
+import freemind.modes.MindMap.MapSourceChangedObserver;
+import freemind.modes.common.GotoLinkNodeAction;
+import freemind.modes.mindmapmode.actions.ApplyPatternAction;
+import freemind.modes.mindmapmode.actions.ChangeArrowsInArrowLinkAction;
+import freemind.modes.mindmapmode.actions.ColorArrowLinkAction;
 import freemind.modes.mindmapmode.actions.HookAction;
+import freemind.modes.mindmapmode.actions.MindMapActions;
+import freemind.modes.mindmapmode.actions.RemoveArrowLinkAction;
 import freemind.modes.mindmapmode.hooks.MindMapHookFactory;
 
 public class MindMapMenuController
@@ -27,6 +50,11 @@ public class MindMapMenuController
 	
 	private MindMapController mindMapController;
 	private MenuStructure mMenuStructure;
+	private MindMapPopupMenu popupmenu;
+	private MindMapToolBar toolbar;
+	private Vector hookActions;
+	private Vector iconActions;
+	private ApplyPatternAction patterns[];
 	
 	public MindMapMenuController(MindMapController mindMapController)
 	{
@@ -41,6 +69,11 @@ public class MindMapMenuController
 	
 	private void init()
 	{	
+		hookActions = mindMapController.getHookActions();
+		iconActions = mindMapController.iconActions;
+		toolbar = mindMapController.getToolBar();
+		patterns = mindMapController.patterns;
+		
 		logger.info("mindmap_menus");
 		// load menus:
 		try {
@@ -51,6 +84,9 @@ public class MindMapMenuController
 			// TODO Auto-generated catch block
 			freemind.main.Resources.getInstance().logException(e);
 		}
+		
+		logger.info("MindMapPopupMenu");
+		popupmenu = new MindMapPopupMenu(mindMapController);
 	}
 	
 	public void updateMenus(StructuredMenuHolder holder) {
@@ -62,7 +98,7 @@ public class MindMapMenuController
 																			 */
 		// add hook actions to this holder.
 		// hooks, fc, 1.3.2004:
-		MindMapHookFactory hookFactory = (MindMapHookFactory) getHookFactory();
+		MindMapHookFactory hookFactory = (MindMapHookFactory) mindMapController.getHookFactory();
 		for (int i = 0; i < hookActions.size(); ++i) {
 			AbstractAction hookAction = (AbstractAction) hookActions.get(i);
 			String hookName = ((HookAction) hookAction).getHookName();
@@ -87,6 +123,32 @@ public class MindMapMenuController
 
 	}
 
+	public void addIconsToMenu(StructuredMenuHolder holder,
+			String iconMenuString) {
+		JMenu iconMenu = holder.addMenu(new JMenu(getText("icon_menu")),
+				iconMenuString + "/.");
+		holder.addAction(removeLastIconAction, iconMenuString
+				+ "/removeLastIcon");
+		holder.addAction(removeAllIconsAction, iconMenuString
+				+ "/removeAllIcons");
+		holder.addSeparator(iconMenuString);
+		for (int i = 0; i < iconActions.size(); ++i) {
+			JMenuItem item = holder.addAction((Action) iconActions.get(i),
+					iconMenuString + "/" + i);
+		}
+	}
+	
+	public void createPatternSubMenu(StructuredMenuHolder holder,
+			String formatMenuString) {
+		for (int i = 0; i < patterns.length; ++i) {
+			JMenuItem item = holder.addAction(patterns[i], formatMenuString
+					+ "patterns/patterns/" + i);
+			item.setAccelerator(KeyStroke
+					.getKeyStroke(getFrame().getAdjustableProperty(
+							"keystroke_apply_pattern_" + (i + 1))));
+		}
+	}
+	
 	public MenuStructure updateMenusFromXml(InputStream in) {
 		// get from resources:
 		try {
@@ -102,18 +164,134 @@ public class MindMapMenuController
 		}
 	}
 
-	public void addIconsToMenu(StructuredMenuHolder holder,
-			String iconMenuString) {
-		JMenu iconMenu = holder.addMenu(new JMenu(getText("icon_menu")),
-				iconMenuString + "/.");
-		holder.addAction(removeLastIconAction, iconMenuString
-				+ "/removeLastIcon");
-		holder.addAction(removeAllIconsAction, iconMenuString
-				+ "/removeAllIcons");
-		holder.addSeparator(iconMenuString);
-		for (int i = 0; i < iconActions.size(); ++i) {
-			JMenuItem item = holder.addAction((Action) iconActions.get(i),
-					iconMenuString + "/" + i);
+	public void processMenuCategory(StructuredMenuHolder holder, List list,
+			String category) {
+		String categoryCopy = category;
+		ButtonGroup buttonGroup = null;
+		for (Iterator i = list.iterator(); i.hasNext();) {
+			Object obj = (Object) i.next();
+			if (obj instanceof MenuCategoryBase) {
+				MenuCategoryBase cat = (MenuCategoryBase) obj;
+				String newCategory = categoryCopy + "/" + cat.getName();
+				holder.addCategory(newCategory);
+				if (cat instanceof MenuSubmenu) {
+					MenuSubmenu submenu = (MenuSubmenu) cat;
+					holder.addMenu(new JMenu(getText(submenu.getNameRef())),
+							newCategory + "/.");
+				}
+				processMenuCategory(holder, cat.getListChoiceList(),
+						newCategory);
+			} else if (obj instanceof MenuActionBase) {
+				MenuActionBase action = (MenuActionBase) obj;
+				String field = action.getField();
+				String name = action.getName();
+				if (name == null) {
+					name = field;
+				}
+				String keystroke = action.getKeyRef();
+				try {
+					Action theAction = (Action) Tools.getField(new Object[] {
+							this, getController() }, field);
+					String theCategory = categoryCopy + "/" + name;
+					if (obj instanceof MenuCheckedAction) {
+						addCheckBox(holder, theCategory, theAction, keystroke);
+					} else if (obj instanceof MenuRadioAction) {
+						final JRadioButtonMenuItem item = (JRadioButtonMenuItem) addRadioItem(
+								holder, theCategory, theAction, keystroke,
+								((MenuRadioAction) obj).getSelected());
+						if (buttonGroup == null)
+							buttonGroup = new ButtonGroup();
+						buttonGroup.add(item);
+
+					} else {
+						add(holder, theCategory, theAction, keystroke);
+					}
+				} catch (Exception e1) {
+					freemind.main.Resources.getInstance().logException(e1);
+				}
+			} else if (obj instanceof MenuSeparator) {
+				holder.addSeparator(categoryCopy);
+			} /* else exception */
 		}
 	}
+
+	public JPopupMenu getPopupMenu() {
+		return popupmenu;
+	}
+
+	/**
+	 * Link implementation: If this is a link, we want to make a popup with at
+	 * least removelink available.
+	 */
+	public JPopupMenu getPopupForModel(java.lang.Object obj) {
+		if (obj instanceof MindMapArrowLinkModel) {
+			// yes, this is a link.
+			MindMapArrowLinkModel link = (MindMapArrowLinkModel) obj;
+			JPopupMenu arrowLinkPopup = new JPopupMenu();
+			// block the screen while showing popup.
+			arrowLinkPopup.addPopupMenuListener(this.popupListenerSingleton);
+			removeArrowLinkAction.setArrowLink(link);
+			arrowLinkPopup.add(new RemoveArrowLinkAction(this, link));
+			arrowLinkPopup.add(new ColorArrowLinkAction(this, link));
+			arrowLinkPopup.addSeparator();
+			/* The arrow state as radio buttons: */
+			JRadioButtonMenuItem itemnn = new JRadioButtonMenuItem(
+					new ChangeArrowsInArrowLinkAction(this, "none",
+							"images/arrow-mode-none.png", link, false, false));
+			JRadioButtonMenuItem itemnt = new JRadioButtonMenuItem(
+					new ChangeArrowsInArrowLinkAction(this, "forward",
+							"images/arrow-mode-forward.png", link, false, true));
+			JRadioButtonMenuItem itemtn = new JRadioButtonMenuItem(
+					new ChangeArrowsInArrowLinkAction(this, "backward",
+							"images/arrow-mode-backward.png", link, true, false));
+			JRadioButtonMenuItem itemtt = new JRadioButtonMenuItem(
+					new ChangeArrowsInArrowLinkAction(this, "both",
+							"images/arrow-mode-both.png", link, true, true));
+			itemnn.setText(null);
+			itemnt.setText(null);
+			itemtn.setText(null);
+			itemtt.setText(null);
+			arrowLinkPopup.add(itemnn);
+			arrowLinkPopup.add(itemnt);
+			arrowLinkPopup.add(itemtn);
+			arrowLinkPopup.add(itemtt);
+			// select the right one:
+			boolean a = !link.getStartArrow().equals("None");
+			boolean b = !link.getEndArrow().equals("None");
+			itemtt.setSelected(a && b);
+			itemnt.setSelected(!a && b);
+			itemtn.setSelected(a && !b);
+			itemnn.setSelected(!a && !b);
+
+			arrowLinkPopup.addSeparator();
+
+			arrowLinkPopup.add(new GotoLinkNodeAction(this, link.getSource()));
+			arrowLinkPopup.add(new GotoLinkNodeAction(this, link.getTarget()));
+
+			arrowLinkPopup.addSeparator();
+			// add all links from target and from source:
+			HashSet NodeAlreadyVisited = new HashSet();
+			NodeAlreadyVisited.add(link.getSource());
+			NodeAlreadyVisited.add(link.getTarget());
+			Vector links = getMindMapMapModel().getLinkRegistry().getAllLinks(
+					link.getSource());
+			links.addAll(getMindMapMapModel().getLinkRegistry().getAllLinks(
+					link.getTarget()));
+			for (int i = 0; i < links.size(); ++i) {
+				MindMapArrowLinkModel foreign_link = (MindMapArrowLinkModel) links
+						.get(i);
+				if (NodeAlreadyVisited.add(foreign_link.getTarget())) {
+					arrowLinkPopup.add(new GotoLinkNodeAction(this,
+							foreign_link.getTarget()));
+				}
+				if (NodeAlreadyVisited.add(foreign_link.getSource())) {
+					arrowLinkPopup.add(new GotoLinkNodeAction(this,
+							foreign_link.getSource()));
+				}
+			}
+			return arrowLinkPopup;
+		}
+		return null;
+	}
+
 }
