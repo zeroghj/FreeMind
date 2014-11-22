2473aaf417b1b85c213d8609094bb5370aa9cc6a
Kevin Coutu <kevin.tcoutu@gmail.com>
2014-11-22 15:48:25 -0500

    Added the visitor SetColorActionVisitor implementing the IActionVistor interface. The visit methods are still empty.

---
+++ freemind/modes/mindmapmode/actions/SetColorActionVisitor.java
@@ -0,0 +1,15 @@
+package freemind.modes.mindmapmode.actions;
+
+public class SetColorActionVisitor implements IActionVisitor{
+	public void visit(CloudColorAction cloudColorAct){
+		
+	}
+	
+	public void visit(EdgeColorAction edgeColorAct){
+		
+	}
+	
+	public void visit(NodeColorAction nodeColorAct){
+		
+	}
+}
